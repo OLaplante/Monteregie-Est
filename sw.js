7a0655859bbb87d83090991c16e471a4b7fc3ec6
OLaplante <o.laplante27@gmail.com>
2026-08-23 16:15:47 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker autonome du brouillon Montérégie-Est.
 // Ce cache est indépendant de celui du site officiel trouvetaclinique.ca.
-const CACHE = 'ptem-2027-monteregie-est-draft-v5';
+const CACHE = 'ptem-2027-monteregie-est-draft-v6';
 const CORE = [
   './',
   './index.html',
